895cf3be28bf7f1bd1c8be796b6756ff205c6266
Casper Holm Bach <Casper-hb@hotmail.dk>
2025-12-17 20:54:01 +0100

    Finished user controller + minor change in AuthController

--- Tasklane/app/Http/Controllers/UserController.php
+++ Tasklane/app/Http/Controllers/UserController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Users;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -18,9 +19,45 @@ class UserController extends Controller
     // Get projects and tasks where user is foreign key
     public function show(User $user)
     {
-        // Eager load to avoid N + 1
+        // Eager load all relations up form to avoid N + 1. 
+        // Eager loading is useful when expecting to using all data.
         $user->load(['projects', 'assignedTasks']);
 
+        // Redirect back to profile
         return view('users.show', compact('user'));
+    }
+
+    public function update(Request $request, User $user)
+    {
+        // Validate request
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . $user->id,
+        ]);
+
+        // Apply change
+        $user->update($data);
+
+        // Redirect back to profile with succes message
+        return redirect()->route('users.show', $user)->with('success', 'Profile updated successfully.');
+    }
+
+    public function destroy(Request $request, User $user)
+    {
+        // Check if the user performing the delete is the user being deleted
+        if(auth()->id() !== $user->id)
+        {
+            abort(403, 'Unauthorized action.');
+        }
+
+        $user->delete();
+
+        // Logout, destroy session, and regenerate CSRF token
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        // Redirect to public page
+        return redirect('/')->with('success', 'Your account has been deleted');
     }
 }
