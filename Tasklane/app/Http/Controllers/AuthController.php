fee5e5e705765467b84a3e022411becc80e64083
Casper Holm Bach <Casper-hb@hotmail.dk>
2025-12-18 20:36:34 +0100

    Missing inheritance from Controller

--- Tasklane/app/Http/Controllers/AuthController.php
+++ Tasklane/app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class AuthController
+class AuthController extends Controller
 {
     public function register(Request $request)
     {
