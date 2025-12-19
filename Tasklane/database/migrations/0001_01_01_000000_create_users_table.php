f3e92443deac6a0eaee481fabb295d5cfba0a6dd
Casper Holm Bach <Casper-hb@hotmail.dk>
2025-12-19 07:36:56 +0100

    Added missing session table

--- Tasklane/database/migrations/0001_01_01_000000_create_users_table.php
+++ Tasklane/database/migrations/0001_01_01_000000_create_users_table.php
@@ -8,6 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
+        // 1. Create Users Table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -17,10 +18,29 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        // 2. Create Password Reset Tokens Table
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        // 3. Create Sessions Table
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
